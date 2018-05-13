251e052b38a66abac96ba3abf8207c7427333eb4
kjisoo <kim@jisoo.net>
2018-05-13 23:20:55 +0900

    Implement TOPParser

--- naver-music-for-mac/Core/Parser/TOPParser.swift
+++ naver-music-for-mac/Core/Parser/TOPParser.swift
@@ -11,11 +11,62 @@ import Kanna
 
 
 protocol TOPParserType {
-  func parse(from htmlString: String) -> [(music: Music, album: Album?, artist: Artist?)]
+  associatedtype E = [(music: Music, album: Album?, artist: Artist?)]
+  func parse(from htmlString: String) -> E
 }
 
 class TOPParser: TOPParserType {
-  func parse(from htmlString: String) -> [(music: Music, album: Album?, artist: Artist?)] {
-    return []
+  private func getQueryStringParameter(url: String, param: String) -> String? {
+    guard let url = URLComponents(string: url) else { return nil }
+    return url.queryItems?.first(where: { $0.name == param })?.value
+  }
+
+  func parse(from htmlString: String) -> E {
+    guard let doc = try? HTML(html: htmlString, encoding: .utf8) else {
+      return []
+    }
+    var musics: E = []
+    for li in doc.css("tr._tracklist_move") {
+      if let _ = li.css("td.ranking").first?.text {
+        var music: Music? = nil
+        var album: Album? = nil
+        var artist: Artist? = nil
+        
+        for a in li.css("a") {
+          if let href = a["href"] {
+            if href.contains("albumId"),
+              let id = getQueryStringParameter(url: href, param: "albumId"),
+              let name = a.css("img").first?["title"] {
+              album = Album()
+              album?.id = id
+              album?.name = name
+            }
+            
+            if href.contains("artistId"),
+              let id = getQueryStringParameter(url: href, param: "artistId"),
+              let name = a["title"] {
+              artist = Artist()
+              artist?.id = id
+              artist?.name = name
+            }
+            
+            if (a.className ?? "").contains("title"),
+              let name = a["title"] {
+              music = Music()
+              music?.id = String(href.dropFirst())
+              music?.name = name
+            }
+          }
+        }
+        
+        if let music = music {
+          music.album = album
+          music.artist = artist
+          album?.artist = artist
+          musics.append((music: music, album: album, artist: artist))
+        }
+      }
+    }
+    return musics
   }
 }
