c218b39d457a9806b42d1402b2a4e1e9a4ce7204
kjisoo <kim@jisoo.net>
2018-05-15 00:58:12 +0900

    Add logic to create album cover url

--- naver-music-for-mac/Models/Album.swift
+++ naver-music-for-mac/Models/Album.swift
@@ -10,13 +10,38 @@ import Foundation
 import RealmSwift
 
 class Album: Object {
+  enum Size {
+    case small
+    case medium
+    case large
+  }
+  
   @objc dynamic var id: String?
   @objc dynamic var name: String?
-  @objc dynamic var coverImageURL: String?
   @objc dynamic var artist: Artist?
   let musics = LinkingObjects(fromType: Music.self, property: "album")
   
   override class func primaryKey() -> String? {
     return "id"
+  }
+  
+  public func coverImageURL(size: Size) -> URL?{
+    guard let idString = self.id,
+      let id = Int(idString) else {
+        return nil
+    }
+    var url = "http://musicmeta.phinf.naver.net"
+    
+    url += String(format: "/album/%03d/%03d/%@.jpg", id / 1000000, (id % 1000000) / 1000, idString)
+    
+    switch size {
+    case .small:
+      url += "?type=r32Fll"
+    case .medium:
+      url += "?type=r104Fll"
+    case .large:
+      url += "?type=r204Fll"
+    }
+    return URL(string: url)
   }
 }
---
+++ naver-music-for-macTests/Models/AlbumSpec.swift
@@ -0,0 +1,61 @@
+//
+//  AlbumSpec.swift
+//  naver-music-for-macTests
+//
+//  Created by A on 2018. 5. 15..
+//  Copyright © 2018년 Kimjisoo. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import naver_music_for_mac
+
+class AlbumSpec: QuickSpec {
+  override func spec() {
+    context("Cover image url by id") {
+      it("id of album is empty") {
+        let album = Album()
+        
+        expect(album.coverImageURL(size: .small)).to(beNil())
+      }
+      
+      it("length of id is six") {
+        let album = Album()
+        album.id = "123456"
+        
+        expect(album.coverImageURL(size: .small)!).to(equal(URL(string: "http://musicmeta.phinf.naver.net/album/000/123/123456.jpg?type=r32Fll")))
+      }
+      
+      it("length of id is seven") {
+        let album = Album()
+        album.id = "1234567"
+        
+        expect(album.coverImageURL(size: .small)!).to(equal(URL(string: "http://musicmeta.phinf.naver.net/album/001/234/1234567.jpg?type=r32Fll")))
+      }
+    }
+    
+    context("size of cover image") {
+      it("small") {
+        let album = Album()
+        album.id = "123456"
+        
+        expect(album.coverImageURL(size: .small)!).to(equal(URL(string: "http://musicmeta.phinf.naver.net/album/000/123/123456.jpg?type=r32Fll")))
+      }
+      
+      it("medium") {
+        let album = Album()
+        album.id = "123456"
+        
+        expect(album.coverImageURL(size: .medium)!).to(equal(URL(string: "http://musicmeta.phinf.naver.net/album/000/123/123456.jpg?type=r104Fll")))
+      }
+      
+      it("large") {
+        let album = Album()
+        album.id = "123456"
+        
+        expect(album.coverImageURL(size: .large)!).to(equal(URL(string: "http://musicmeta.phinf.naver.net/album/000/123/123456.jpg?type=r204Fll")))
+      }
+    }
+  }
+}
