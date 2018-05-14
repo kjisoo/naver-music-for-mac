fee4a4baec5903dbd7f9a6f56f3cd7f2fa5d369a
kjisoo <kim@jisoo.net>
2018-05-14 18:42:18 +0900

    Add fetch TOP100 logic from online

--- naver-music-for-mac/Core/MusicBrowser.swift
+++ naver-music-for-mac/Core/MusicBrowser.swift
@@ -8,14 +8,38 @@
 
 import Foundation
 import RxSwift
+import Moya
 
-enum TOPType {
-  case total
-  case domestic
-  case oversea
-  case musician
+enum TOPType: String {
+  case total = "TOTAL"
+  case domestic = "DOMESTIC"
+  case oversea = "OVERSEA"
 }
 
 protocol MusicBrowserType {
   func search(top type: TOPType) -> Single<[Music]>
+}
+
+class MusicBrowser: MusicBrowserType {
+  private let provider: MoyaProvider<NaverPage>
+  private let topParser: TOPParser
+
+  init(provider: MoyaProvider<NaverPage>, parser: TOPParser = TOPParser()) {
+    self.provider = provider
+    self.topParser = parser
+  }
+  
+  func search(top type: TOPType) -> Single<[Music]> {
+    return Single.zip(self.provider.rx.request(.top(domain: type.rawValue, page: 1)),
+                      self.provider.rx.request(.top(domain: type.rawValue, page: 2)))
+      .map { [weak self] (firstResponse, secondResponse) -> [Music]  in
+        if let `self` = self,
+          let firstResponseString = String(data: firstResponse.data, encoding: .utf8),
+          let secondResponseString = String(data: secondResponse.data, encoding: .utf8) {
+          let result = self.topParser.parse(from: firstResponseString) + self.topParser.parse(from: secondResponseString)
+          return result.map { $0.music }
+        }
+        return []
+    }
+  }
 }
