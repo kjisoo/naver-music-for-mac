62c580645e457478d54b0601bc8be2c68a4a4e8a
kjisoo <kim@jisoo.net>
2018-05-16 18:54:24 +0900

    Add PlayerService

--- naver-music-for-mac/Core/PlayerService.swift
+++ naver-music-for-mac/Core/PlayerService.swift
@@ -6,11 +6,40 @@
 //  Copyright © 2018년 Kimjisoo. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+import WebKit
 
 protocol PlayerServiceType {
+  var playList: Playlist { get }
   func resume()
   func pause()
   func next()
   func prev()
+  func play(index: Int)
+}
+
+class PlayerService: NSObject {
+  let webPlayer: WebView = {
+    return WebView()
+  }()
+  
+  override init() {
+    super.init()
+    setupWebPlayer()
+  }
+  
+  private func setupWebPlayer() {
+    self.webPlayer.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+    self.webPlayer.mainFrame.load(URLRequest(url: URL(string: "http://m.music.naver.com")!))
+    self.webPlayer.uiDelegate = self
+    self.webPlayer.frameLoadDelegate = self
+  }
+}
+
+extension PlayerService: WebUIDelegate {
+  
+}
+
+extension PlayerService: WebFrameLoadDelegate {
+  
 }
