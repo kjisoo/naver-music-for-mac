ed3b7ff7e8e32275319fc12bb731f382b8756f79
kjisoo <kim@jisoo.net>
2018-05-17 15:21:19 +0900

    Add realm in container of Swinject

--- naver-music-for-mac/AppDelegate.swift
+++ naver-music-for-mac/AppDelegate.swift
@@ -7,14 +7,25 @@
 //
 
 import Cocoa
+import Swinject
+import RealmSwift
+import Moya
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   let window = WindowController()
-
+  let container: Swinject.Container = {
+    let container = Swinject.Container()
+    container.register(Realm.self) { _ in try! Realm() }
+    container.register(MoyaProvider<NaverPage>.self) { _ in MoyaProvider<NaverPage>() }
+    container.register(TOPParser.self) { _ in TOPParser() }
+    return container
+  }()
+  
   override func awakeFromNib() {
     super.awakeFromNib()
+    Container.container = container
     window.showWindow(nil)
   }
 
---
+++ naver-music-for-mac/Models/Object+Realm.swift
@@ -0,0 +1,16 @@
+//
+//  Object+Realm.swift
+//  naver-music-for-mac
+//
+//  Created by sonny on 2018. 5. 17..
+//  Copyright © 2018년 Kimjisoo. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+extension Object {
+  var realm: Realm {
+    return Container.container.resolve(Realm.self)!
+  }
+}
