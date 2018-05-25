7817199267cbc471c22730ccba57dd72b0cf0118
kjisoo <kim@jisoo.net>
2018-05-25 11:30:15 +0900

    Show album info in mylist

--- naver-music-for-mac/Core/Parser/MusicListParser.swift
+++ naver-music-for-mac/Core/Parser/MusicListParser.swift
@@ -28,12 +28,24 @@ class MusicListParser: ParserType {
     if let ul = doc.css("ul#addList").first {
       for li in ul.css("li") {
         var list: [String: Any] = [:]
+        var album: [String: Any] = [:]
         if let title = li.css(".tit").first?.text?.trimmingCharacters(in: .whitespacesAndNewlines) {
           list["name"] = title
         }
         if let href = li.css("a.btn_play").first?["onclick"],
           let id = slice(string: href, from: "playByTrackId(", to: ")") {
           list["id"] = id
+        }
+        if let href = li.css("a.img").first?["onclick"],
+          let albumId = slice(string: href, from: "goAlbum(", to: ")") {
+          album["id"] = albumId
+        }
+        if let subTitle = li.css("span.stit").first?.text,
+          let albumName = subTitle.split(separator: "-", maxSplits: 1, omittingEmptySubsequences: false).last {
+          album["name"] = albumName
+        }
+        if album.keys.count > 0 {
+          list["album"] = album
         }
         musicList.append(list)
       }
--- naver-music-for-mac/Screens/MyList/MusicListViewController.swift
+++ naver-music-for-mac/Screens/MyList/MusicListViewController.swift
@@ -9,7 +9,19 @@
 import Cocoa
 
 class MusicListViewController: BaseViewController {
+  private var cellViewModels: [MusicCellViewModel] = []
   private let viewModel: MusicListViewModel
+  
+  private lazy var tableView: NSTableView = {
+    let tableView = NSTableView()
+    tableView.headerView = nil
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.rowHeight = 100
+    tableView.intercellSpacing = NSSize.zero
+    tableView.addTableColumn(NSTableColumn(identifier: NSUserInterfaceItemIdentifier(rawValue: "MusicColumn")))
+    return tableView
+  }()
   
   init(viewModel: MusicListViewModel) {
     self.viewModel = viewModel
@@ -20,9 +32,50 @@ class MusicListViewController: BaseViewController {
     fatalError("init(coder:) has not been implemented")
   }
   
+  override func setupConstraint() {
+    super.setupConstraint()
+    let scrollView = NSScrollView()
+    let clipView = NSClipView()
+    scrollView.contentView = clipView
+    clipView.documentView = self.tableView
+    self.view.addSubview(scrollView)
+    
+    scrollView.snp.makeConstraints { (make) in
+      make.top.equalTo(self.titleLabel.snp.bottom).offset(16)
+      make.bottom.leading.trailing.equalToSuperview()
+    }
+  }
+  
   override func bindWithViewModel() {
     self.viewModel.title.subscribe(onNext: { [weak self] in
       self?.title = $0
     }).disposed(by: self.disposeBag)
+    
+    self.viewModel.musicDatasource.asDriver(onErrorJustReturn: []).drive(onNext: { [weak self] (viewModels) in
+      self?.cellViewModels = viewModels
+      self?.tableView.reloadData()
+      self?.tableView.scrollRowToVisible(0)
+    }).disposed(by: self.disposeBag)
+  }
+}
+
+extension MusicListViewController: NSTableViewDataSource {
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return self.cellViewModels.count
+  }
+  
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "MusicCellView"), owner: self) as? MusicCellView ?? MusicCellView()
+    cell.viewModel = self.cellViewModels[row]
+    return cell
+  }
+}
+
+extension MusicListViewController: NSTableViewDelegate {
+  func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+    if let isChecked = try? self.cellViewModels[row].isChecked.value() {
+      self.cellViewModels[row].isChecked.onNext(!isChecked)
+    }
+    return false
   }
 }
