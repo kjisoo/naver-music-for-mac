e61f9660d600b6226d3c2c29a2d69028eb062045
iisue <sklee@iisue.com>
2018-06-17 11:27:45 +0900

    Add media key support

--- naver-music-for-mac/AppDelegate.swift
+++ naver-music-for-mac/AppDelegate.swift
@@ -10,10 +10,14 @@ import Cocoa
 import RealmSwift
 import Fabric
 import Crashlytics
+import MediaKeyTap
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   var window: WindowController?
+  
+  // MediaKeyTap
+  var mediaKeyTab: MediaKeyTap?
   
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -24,7 +28,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     Fabric.with([Crashlytics.self, Answers.self])
     self.window = WindowController()
     window?.showWindow(nil)
+    
+    // MediaKeyTap
+    self.mediaKeyTab = MediaKeyTap(delegate: self)
+    self.mediaKeyTab?.start()
+    
   }
 
+}
+
+extension AppDelegate: MediaKeyTapDelegate {
+  func handle(mediaKey: MediaKey, event: KeyEvent) {
+    
+    switch mediaKey {
+    case .playPause:
+      let playlist = Playlist.getMyPlayList()
+      playlist.setIsPaused(isPaused: !playlist.isPaused)
+      break
+      
+    case .rewind:
+      Playlist.getMyPlayList().prev()
+      break
+      
+    case .fastForward:
+      Playlist.getMyPlayList().next()
+      break
+      
+    default:
+      break
+    }
+  }
 }
 
